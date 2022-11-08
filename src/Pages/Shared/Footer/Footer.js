5d02782e4186d71807cef0275b02a27dc43e7ce8
hussain322 <hussain.ahmed8768@gmail.com>
2022-11-08 09:29:55 +0600

    added header and footer

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-center text-white">
+      <div className="p-4">copyright 2022 by wild photographer</div>
+    </footer>
+  );
+};
+
+export default Footer;
